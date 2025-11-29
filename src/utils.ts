6c28e29daa5c51df0989d8d967fca350debfd2e5
DevlRitik <ritikbhushan11@gmail.com>
2025-11-29 20:21:00 +0530

    added utils func

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,31 @@
-import fs from "fs";
+import { promises as fs } from "fs";
+
+
+
+export const isFile = async (Path: string): Promise<boolean> => {
+    try {
+        const IsFile = (await fs.stat(Path)).isFile()
+        return IsFile
+
+    } catch (e) {
+        return false
+    }
+}
+
+
+export const isDir = async (Path : string) : Promise<Boolean> => { 
+    try { 
+        const isDir = (await fs.stat(Path)).isDirectory()
+        return isDir
+    }catch(e) {
+        return false
+    }
+}
+
+
+
+
+
+
+
 
